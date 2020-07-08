9c283fceb6b49ee1816004cd2bac77b9ae513b48
businan <yorulmazburaksinan@gmail.com>
2020-07-08 11:16:56 -0700

    changing and to or

--- corona.py
+++ corona.py
@@ -8,7 +8,7 @@ If your answer is 'No' just hit Enter on keyboard
 age = bool(input('Are you a cigarette addict older than 75 years old? '))
 chronic = bool(input('Do you have a severe chronic disease? '))
 immune = bool(input('Is your immune system too weak? '))
-if age and chronic and immune :
+if age or chronic or immune :
     print('\nYou are in risky group')
 else:
     print('\nYou are not in risky group')
