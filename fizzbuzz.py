b8eb1f4f2087968ff1de856fad1a5caa851d3293
businan <yorulmazburaksinan@gmail.com>
2020-07-17 01:02:48 +0200

    change fizzbuzz to function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,13 @@
-for i in range(1,101):
-    if i % 15 == 0 :
-        print(i, 'is FizzBuzz')
-    elif i % 3 == 0 :
-        print(i, 'is Fizz')
-    elif i % 5 == 0 :
-        print(i, 'is Buzz')
-    else:
-        print(i, 'is unchanged')
+def fizzbuzz(x, y):
+    for i in range(x, y):
+        if i % 15 == 0 :
+            print(i, 'is FizzBuzz')
+        elif i % 3 == 0 :
+            print(i, 'is Fizz')
+        elif i % 5 == 0 :
+            print(i, 'is Buzz')
+        else:
+            print(i, 'is unchanged')
+start = int(input("Enter number that you want to start searching in FizzBuzz : "))
+stop = int(input("Enter number that you want to finish searching in FizzBuzz : "))
+fizzbuzz(start,stop)
